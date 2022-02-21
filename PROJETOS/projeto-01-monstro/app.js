19e01e09f5f38362e0dc6852284591cf6525fc06
Vinicius Oliveira <vine0@hotmail.com>
2022-02-21 13:10:16 -0300

    Finalização do projeto Matador de Monstros

--- PROJETOS/projeto-01-monstro/app.js
+++ PROJETOS/projeto-01-monstro/app.js
@@ -4,6 +4,7 @@ new Vue({
         running: false,
         playerLife: 100,
         monsterLife: 100,
+        logs: []
     },
     computed: {
         hasResult(){
@@ -15,27 +16,35 @@ new Vue({
             this.running = true
             this.playerLife = 100
             this.monsterLife = 100
+            this.logs = []
         },
         attack(especial){
-            this.hurt('playerLife',7, 12, especial) // A aleatoriedade do player é maior
-            this.hurt('monsterLife',3, 17, false)
+            this.hurt('monsterLife',7, 12, especial, 'Jogador', 'Monstro', 'player') // A aleatoriedade do player é maior
+            //if(this.monsterLife > 0){
+                this.hurt('playerLife',3, 17, false, 'Monstro', 'Jogador', 'monster')
+            //}
         },
-        hurt(atr, min, max, especial){
+        hurt(atr, min, max, especial, source, target, cls){
             let plus = especial ? 5 : 0
             let hurt = this.getRandom(min + plus, max + plus)
             this[atr] = Math.max(this[atr] - hurt, 0) // faz com que o Life não passe para menos de 0
+            this.registerLog(`${source} atingiu ${target} com ${hurt}.`, cls)
         },
         healAndHurt(){
             this.heal(10, 15)
-            this.hurt('playerLife', 7, 12, false)
+            this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
         },
         heal(max, min){
             let heal = this.getRandom(min, max)
             this.playerLife = Math.min(this.playerLife + heal, 100) // Limita a cura
+            this.registerLog(`Jogador ganhou cura de ${heal}.`, 'player')
         },
         getRandom(min, max){
             let value = Math.random() * (max - min) + min
             return Math.round(value)
+        },
+        registerLog(text, cls) {
+            this.logs.unshift({ text, cls })
         }
     },
     watch: {
